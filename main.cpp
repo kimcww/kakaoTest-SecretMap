1abc6dcaaae7126bc8933f092647d42c0c2f0c6d
KIMCW <kimcww@github.com>
2019-05-24 00:23:46 +0900

    convert dec to binary

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
-#include <QCoreApplication>
+#include<iostream>
 
-int main(int argc, char *argv[])
+int nCount = 0;
+
+int *binary(int input){
+
+    static int Output[] = {0,};
+
+    for(int i = nCount; i >0 ; i--){
+        Output[i-1] = input % 2;
+        input = input/2;
+    }
+
+    return Output;
+}
+int main()
 {
-    QCoreApplication a(argc, argv);
+    std::cin>>nCount;
+    int nArr1[nCount];
+    int nArr2[nCount];
+    for(int i = 0; i<nCount ; i++){
+        std::cin>>nArr1[nCount];
+    }
 
-    return a.exec();
+    for(int i = 0; i<nCount ; i++){
+        std::cin>>nArr2[nCount];
+    }
+
+
+    return -1;
 }
