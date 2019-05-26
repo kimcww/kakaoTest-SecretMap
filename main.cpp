d562b9a2af9195f80da084b06efc7b6bceb9f5f0
KIMCW <kimcww@github.com>
2019-05-26 10:42:11 +0900

    Solved

--- main.cpp
+++ main.cpp
@@ -10,8 +10,31 @@ int *binary(int input){
         Output[i-1] = input % 2;
         input = input/2;
     }
-
     return Output;
+}
+
+int *Calculate(int *Arr1 , int *Arr2){
+
+    static int Calculate[] = {0,};
+    for(int i = 0 ; i < nCount ; i++){
+        Calculate[i] = (Arr1[i] | Arr2[i]);
+
+    }
+    return Calculate;
+}
+
+void Result(int *arr){
+    char Calculate[nCount];
+    for(int i= 0; i < nCount ; i++){
+        if( arr[i] == 1){
+            Calculate[i] = '#';
+        } else {
+            Calculate[i] = ' ';
+        }
+        std::cout<<Calculate[i];
+    }
+    std::cout<<"\n";
+    return;
 }
 int main()
 {
@@ -19,13 +42,13 @@ int main()
     int nArr1[nCount];
     int nArr2[nCount];
     for(int i = 0; i<nCount ; i++){
-        std::cin>>nArr1[nCount];
+        std::cin>>nArr1[i];
     }
-
     for(int i = 0; i<nCount ; i++){
-        std::cin>>nArr2[nCount];
+        std::cin>>nArr2[i];
     }
-
-
+    for( int i = 0; i<nCount; i++){
+        Result( binary(Calculate(nArr1 , nArr2)[i]));
+    }
     return -1;
 }
